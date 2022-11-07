4cb088e5e059b44fbdba98fdd18f968635a291b8
wkdckd41 <wkdckd41@gmail.com>
2022-11-07 22:49:39 +0900

    회원가입 - SecurityConfig 생성

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당 파일로 시큐리티를 활성화
+@Configuration // loc
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화 상태가됨
+	}
+}
