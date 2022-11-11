107a8106df27ed4845705ddbd7389eddf33797de
wkdckd41 <wkdckd41@gmail.com>
2022-11-11 21:46:13 +0900

    회원가입 - SecurityConfig 생성

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -15,6 +15,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		// super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화 상태가됨
 		http.authorizeRequests()
 			.antMatchers("/","/user/**","/image/**","/subscribe/**","/,comment/**").authenticated()
-			.anyRequest().permitAll();
+			.anyRequest().permitAll()
+			.and()
+			.formLogin()
+			.loginPage("/auth/signin")
+			.defaultSuccessUrl("/");
 	}
 }
