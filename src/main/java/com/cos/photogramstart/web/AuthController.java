41cd5051e544f16b449e4237939f84c76deebc28
wkdckd41 <wkdckd41@gmail.com>
2022-11-14 22:34:41 +0900

    회원가입 - User 모델 만들기

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+
 @Controller // 1. IoC 2. 파일을 리턴하는 컨트롤러
 public class AuthController {
 
@@ -17,9 +19,9 @@ public class AuthController {
 		return"auth/signup";
 	}
 	// 회원가입버튼 -> /auth/signup -> /auth/signin
+	// 회원가입버튼 X
 	@PostMapping("/auth/signup")
-	public String signup() {
-		System.out.println("singup실행됨?");
+	public String signup(SignupDto signupdto) { // key = value (x-www-form-urlencoded)
 		
 		return"auth/signin";
 	}
