a50924a34a45d095d9b1c399b20b5f780368d2e6
wkdckd41 <wkdckd41@gmail.com>
2022-11-20 22:49:52 +0900

    회원가입 - 비밀번호 해시

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -2,6 +2,7 @@ package com.cos.photogramstart.domain.user;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -25,6 +26,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
 	private int id;
 	
+	@Column(unique=true)
 	private String username;
 	private String password;
 	
--- src/main/java/com/cos/photogramstart/service/Authservice.java
+++ src/main/java/com/cos/photogramstart/service/Authservice.java
@@ -1,6 +1,8 @@
 package com.cos.photogramstart.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
@@ -12,9 +14,15 @@ import lombok.RequiredArgsConstructor;
 public class Authservice {
 	
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 	
+	@Transactional // Write(Insert, Update, Delete)
 	public User 회원가입(User user) {
 		// 회원가입 진행
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		user.setRole("ROLE_USER"); // 관리자 ROLE_ADMIN
 		User userEntity = userRepository.save(user);
 		return userEntity;
 	}
