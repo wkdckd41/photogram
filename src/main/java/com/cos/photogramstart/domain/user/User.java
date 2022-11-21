6e6c3b60c258c97f14b5b63420588f350e6d9499
wkdckd41 <wkdckd41@gmail.com>
2022-11-21 23:58:36 +0900

    회원가입 - 전처리 후처리(닉네임 20자까지)

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -26,7 +26,7 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
 	private int id;
 	
-	@Column(unique=true)
+	@Column(length =20, unique=true)
 	private String username;
 	private String password;
 	
