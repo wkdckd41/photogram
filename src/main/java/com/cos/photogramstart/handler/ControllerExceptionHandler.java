6a9b52cccc53af29212d4969a5e5740931f8742a
wkdckd41 <wkdckd41@gmail.com>
2022-11-30 23:55:41 +0900

    회원가입 - 글로벌 예외처리 하기

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController
 @ControllerAdvice
 public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(CustomValidationException.class)
-	public Map<String, String> validationException(CustomValidationException e) {
-		return e.getErrorMap();
+	public CMRespDto<?> validationException(CustomValidationException e) {
+		return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
 	}
 
 }
