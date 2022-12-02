c0a30f9c353dc8b8dce13022e97533d585b23aad
wkdckd41 <wkdckd41@gmail.com>
2022-12-02 22:12:57 +0900

    공통 응답 Script 만들기

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,21 +1,21 @@
 package com.cos.photogramstart.handler;
 
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
-import com.cos.photogramstart.web.dto.CMRespDto;
+import com.cos.photogramstart.util.Script;
 
 @RestController
 @ControllerAdvice
 public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(CustomValidationException.class)
-	public CMRespDto<?> validationException(CustomValidationException e) {
-		return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
+	public String validationException(CustomValidationException e) {
+		
+		
+		return Script.back(e.getErrorMap().toString());
 	}
 
 }
--- src/main/java/com/cos/photogramstart/util/Script.java
+++ src/main/java/com/cos/photogramstart/util/Script.java
@@ -2,7 +2,7 @@ package com.cos.photogramstart.util;
 
 public class Script {
 
-	public String back() {
+	public static String back(String msg) {
 		StringBuffer sb = new StringBuffer();
 		sb.append("<script>");
 		sb.append("alert('"+msg+"');");
