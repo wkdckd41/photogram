514ec10c0b0d6c1399df78889abe96fa1283fd9e
wkdckd41 <wkdckd41@gmail.com>
2022-12-05 23:46:42 +0900

    로그인 - 완료

--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -5,13 +5,28 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service // IoC
 public class PrincipalDetailsService implements UserDetailsService{
+	
+	private final UserRepository userRepository;
 
+	// 1. 패스워드는 알아서 체킹하니까 신경쓸 필요 없다.
+	// 2. 리턴이 잘되면 자동으로 UserDetails 타입을 세션으로 만든다.
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		System.out.println("나 실행돼?" +username);
-		return null;
+		
+		User userEntity = userRepository.findByusername(username);
+		
+		if(userEntity == null) {
+			return null;
+		}else{
+			return new PrincipalDetails(userEntity);
+		}
 	}
-
 }
