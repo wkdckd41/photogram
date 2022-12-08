02c2a4c3ef9aa29fa2a97298c3b2823e3ac625d1
wkdckd41 <wkdckd41@gmail.com>
2022-12-08 23:43:38 +0900

    로그인 - View 연결하기

--- src/main/java/com/cos/photogramstart/web/ImageController.java
+++ src/main/java/com/cos/photogramstart/web/ImageController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ImageController {
 
-	@GetMapping({"/"," /image/story"})
+	@GetMapping({"/", "/image/story"})
 	public String story() {
 		return "image/story";
+	}
+	
+	@GetMapping({"image/popular"})
+	public String popular() {
+		return "image/popular";
+	}
+	
+	@GetMapping({"image/upload"})
+	public String upload() {
+		return "image/upload";
 	}
 }
