eef1f69b7a35e7a07e3b402968c1b5d002bcb17c
wkdckd41 <wkdckd41@gmail.com>
2022-12-16 23:36:40 +0900

    회원정보수정 - Ajax 사용하기

---
+++ src/main/java/com/cos/photogramstart/web/dto/user/UserUpdateDto.java
@@ -0,0 +1,27 @@
+package com.cos.photogramstart.web.dto.user;
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateDto {
+	private String name; // 필수값
+	private String password; // 필수값
+	private String website;
+	private String bio;
+	private String phone;
+	private String gender;
+	
+	// 조금 위험함. 코드수정이 필요할 예정
+	public User toEntity() {
+		return User.builder()
+				.name(name)
+				.password(password)
+				.website(website)
+				.bio(bio)
+				.phone(phone)
+				.gender(gender)
+				.build();
+	}
+}
--- src/main/webapp/WEB-INF/views/user/update.jsp
+++ src/main/webapp/WEB-INF/views/user/update.jsp
@@ -22,7 +22,7 @@
 			<!--프로필셋팅 아이디영역end-->
 
 			<!--프로필 수정-->
-			<form id="profileUpdate"">
+			<form id="profileUpdate">
 				<div class="content-item__02">
 					<div class="item__title">이름</div>
 					<div class="item__input">
@@ -73,7 +73,7 @@
 				<div class="content-item__09">
 					<div class="item__title">전회번호</div>
 					<div class="item__input">
-						<input type="text" name="tel" placeholder="전화번호"
+						<input type="text" name="phone" placeholder="전화번호"
 							value="${principal.user.phone}" />
 					</div>
 				</div>
