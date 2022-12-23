53b76584edfc38bcbd961b862d83f0748b195892
wkdckd41 <wkdckd41@gmail.com>
2022-12-23 22:07:22 +0900

    구독하기 - 모델 만들기

--- src/main/java/com/cos/photogramstart/domain/Subscribe/Subscribe.java
+++ src/main/java/com/cos/photogramstart/domain/Subscribe/Subscribe.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import com.cos.photogramstart.domain.user.User;
 
@@ -22,12 +24,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
+@Table(
+		uniqueConstraints = {
+				@UniqueConstraint(
+					name="subscribe_uk",
+					columnNames = {"fromUserId","toUserId"}
+				)
+		}
+)
 public class Subscribe {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
 	private int id;
 	
-	@JoinColumn(name= "fromUserld")
+	@JoinColumn(name= "fromUserld") // 이렇게 컬럼명 만들어! 니 맘대로 만들지 말고!!
 	@ManyToOne
 	private User fromUser;
 	
