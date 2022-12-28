e82b7ff7b5df62078d2b614024f3dc3425677b72
wkdckd41 <wkdckd41@gmail.com>
2022-12-28 23:54:51 +0900

    구독하기 - 구독,구독취소 API 만들기

--- src/main/java/com/cos/photogramstart/domain/Subscribe/SubscribeRepository.java
+++ src/main/java/com/cos/photogramstart/domain/Subscribe/SubscribeRepository.java
@@ -8,9 +8,9 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, Integer>{
 
 	@Modifying // INSERT, DELETE, UPDATE를 네이티브 쿼리로 작성하려면 해당 어노테이션 필요!!
 	@Query(value = "INSERT INTO subscribe(fromUserId, toUserId, createDate) VALUES(:fromUserId, :toUserId, now())", nativeQuery = true)
-	int mSubscribe(int fromUserId, int toUserId); // 1(변경된 행의 개수가 리턴됨), -1
-
+	void mSubscribe(int fromUserId, int toUserId);
+	
 	@Modifying
 	@Query(value = "DELETE FROM subscribe WHERE fromUserId = :fromUserId AND toUserId = :toUserId", nativeQuery = true)
-	int mUnSubscribe(int fromUserId, int toUserId); // 1, -1
+	void mUnSubscribe(int fromUserId, int toUserId);
 }
--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -1,17 +1,27 @@
 package com.cos.photogramstart.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.photogramstart.domain.Subscribe.Subscribe;
+import com.cos.photogramstart.domain.Subscribe.SubscribeRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class SubscribeService {
 	
+	private final SubscribeRepository subscribeRepository ;
+	
+	@Transactional
 	public void 구독하기(int fromUserId, int toUserID) {
-		
+		subscribeRepository.mSubscribe(fromUserId, toUserID);
 	}
 
+	@Transactional
 	public void 구독취소하기(int fromUserId, int toUserID) {
-				
+		subscribeRepository.mUnSubscribe(fromUserId, toUserID);
+
 	}	
 }
