da3b685ab2674af281b5d498f6565651623cd9c0
wkdckd41 <wkdckd41@gmail.com>
2023-01-04 20:53:46 +0900

    프로필 페이지 - Image 서버에 업로드하기

--- src/main/java/com/cos/photogramstart/web/ImageController.java
+++ src/main/java/com/cos/photogramstart/web/ImageController.java
@@ -1,7 +1,12 @@
 package com.cos.photogramstart.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
 
 @Controller
 public class ImageController {
@@ -19,5 +24,12 @@ public class ImageController {
 	@GetMapping({"image/upload"})
 	public String upload() {
 		return "image/upload";
+	}
+	
+	@PostMapping("/image")
+	public String imageUpload(ImageUploadDto imageUploadDto, @AuthenticationPrincipal PrincipalDetails principalDetails ) {
+		// 서비스호출
+		
+		return "";
 	}
 }
