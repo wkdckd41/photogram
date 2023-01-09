add61dce3c6e8624a9975b9a76401aab7320aafe
wkdckd41 <wkdckd41@gmail.com>
2023-01-09 23:59:25 +0900

    프로필 페이지 - Image 서버에 업로드하기

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -1,5 +1,11 @@
 package com.cos.photogramstart.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
@@ -14,7 +20,21 @@ public class ImageService {
 
 	private final ImageRepository imageRepository;
 	
+	@Value("${file.path}")
+	private String uploadFolder;
+	
 	public void 사진업로드(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
-		String imageFileName = imageUploadDto.getFile().getOriginalFilename(); // 1.jpg
+		UUID uuid = UUID.randomUUID(); //uuid
+		String imageFileName = uuid+"_"+imageUploadDto.getFile().getOriginalFilename(); // 1.jpg
+		System.out.println("이미지 파일이름 : "+ imageFileName);
+		
+		Path imageFilePath = Paths.get(uploadFolder+imageFileName);
+		
+		// 통신, I/O -> 예외가 발생할 수 있다.
+		try {
+			Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
