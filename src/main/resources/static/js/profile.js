f4fc7489eb8b6162cc5b8c6fb0de575dc2163a28
wkdckd41 <wkdckd41@gmail.com>
2023-01-12 00:55:07 +0900

    구독정보 뷰 렌더링 - 구독모달 정보 렌더링 완료

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -37,13 +37,46 @@ function toggleSubscribe(toUserId, obj) {
 	}
 }
 
-// (2) 구독자 정보  모달 보기
-function subscribeInfoModalOpen() {
+// (2) 구독자 정보  모달 보기	
+function subscribeInfoModalOpen(pageUserId) {
 	$(".modal-subscribe").css("display", "flex");
+	$.ajax({
+		url: `/api/user/${pageUserId}/subscribe`,
+		dataType: "json"
+	}).done(res => {
+		console.log(res.data);
+
+		res.data.forEach((u) => {
+			let item = getSubscribeModalItem(u);
+			$("#subscribeModalList").append(item);
+		});
+	}).fail(error => {
+		console.log("구독정보 불러오기 오류", error);
+	});
 }
+function getSubscribeModalItem(u) {
 
-function getSubscribeModalItem() {
+	let item = `<div class="subscribe__item" id="subscribeModalItem-${u.id}">	
+	<div class="subscribe__img">	
+		<img src="/upload/${u.profileImageUrl}" onerror="this.src='/images/person.jpeg'" />	
+	</div>	
+	<div class="subscribe__text">	
+		<h2>${u.username}</h2>	
+	</div>	
+	<div class="subscribe__btn">`;
 
+	if (!u.equalUserState) { // 동일 유저가 아닐 때 버튼이 만들이 만들어져야함	
+		if (u.subscribeState) { // 구독한 상태	
+			item += `<button class="cta blue" onclick="toggleSubscribe(${u.id}, this)">구독취소</button>`;
+		} else { // 구독안한 상태	
+			item += `<button class="cta" onclick="toggleSubscribe(${u.id}, this)">구독하기</button>`;
+		}
+	}
+	item += `
+	</div>
+</div>`;
+
+	return item;
 }
 
 
