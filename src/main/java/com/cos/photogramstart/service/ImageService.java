8ee03c749d73a3220ccc6b2e553ba5f53dc6951c
wkdckd41 <wkdckd41@gmail.com>
2023-01-12 18:16:54 +0900

    스토리 페이지 - 페이징하기

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -3,10 +3,11 @@ package com.cos.photogramstart.service;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,8 +25,8 @@ public class ImageService {
 	private final ImageRepository imageRepository;
 	
 	@Transactional(readOnly = true) // 영속성 컨텍스트 변경 감지를 해서, 더티체킹, flush(반영) X
-	public List<Image> 이미지스토리(int principalId){
-		List<Image> images = imageRepository.mStory(principalId);
+	public Page<Image> 이미지스토리(int principalId, Pageable pageable){
+		Page<Image> images = imageRepository.mStory(principalId, pageable);
 		return images;
 	}
 	
