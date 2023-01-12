fde0613a84461687d950d5483053623b6367e022
wkdckd41 <wkdckd41@gmail.com>
2023-01-12 19:10:01 +0900

    좋아요 구현 - Likes 모델 만들기

---
+++ src/main/java/com/cos/photogramstart/domain/likes/Likes.java
@@ -0,0 +1,57 @@
+package com.cos.photogramstart.domain.likes;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+		uniqueConstraints = {
+				@UniqueConstraint(
+						name="likes_uk",
+						columnNames = {"imageId", "UserId"}
+				)
+		}
+)
+public class Likes { // N
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 데이터베이스를 따라간다.
+	private int id;
+	
+	// 오류가 터지고 나서 잡아봅시다.
+	@JoinColumn(name = "imageId")
+	@ManyToOne
+	private Image image; // 1
+	
+	@JoinColumn(name = "userId")
+	@ManyToOne
+	private User user; // 1
+	
+	private LocalDateTime createDate;
+	
+	@PrePersist
+	public void createDate() {
+		this.createDate = LocalDateTime.now();
+	}
+
+}
