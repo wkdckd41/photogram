807c52ce6b978a02df9519b5ad8bf27792261cec
wkdckd41 <wkdckd41@gmail.com>
2023-01-18 18:09:15 +0900

    테스트

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import com.cos.photogramstart.config.oauth.OAuth2DetailsService;
 
 import lombok.RequiredArgsConstructor;
